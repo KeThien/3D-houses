ed121b98b97d6597e31e83160694d0bb0156ac20
AlainTiri <alaintiri@gmail.com>
2021-07-01 15:16:56 +0200

    Changement dans l'interface utilisateur pour encoder les infos immo.

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,6 +1,9 @@
+import os
 from app import app
-from flask import render_template
+import scripts
+from flask import render_template, request, redirect, url_for
 from .weather import RequestWeather
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,5 +22,19 @@ def index():
     ]
     return render_template('index.html', title='3D Houses', user=user, weather=response)
 
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        number = request.form['nm']
+        return redirect(url_for('success', name=number))
+
+
+    else:
+        number = request.args.get('nm')
+        return redirect(url_for('success', name=number))
+
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=5000, debug=True)
+
+
