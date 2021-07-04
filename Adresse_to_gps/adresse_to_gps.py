4d4426802d3ad764e882f1311bf07074829e2944
AlainTiri <alaintiri@gmail.com>
2021-07-04 12:29:07 +0200

    Nettoyage et réorganisation du script

--- Adresse_to_gps/adresse_to_gps.py
+++ Adresse_to_gps/adresse_to_gps.py
@@ -6,52 +6,29 @@ import os
 import geopandas as gpd
 
 
-def dezip(file):
-    file = zipfile.ZipFile('CRAB_Adressenlijst_Shapefile.zip')
-    file.extractall('CRAB_dezip')
+def clear_database(shape_file, ville):
 
-
-def clear_database(shape_file):
-    
-    a = x
-    shape_file = gpd.read_file('CRAB_dezip/Shapefile/CrabAdr.shp')
     shape_file_clear = shape_file[['STRAATNM', 'HUISNR', 'NISCODE', 'GEMEENTE', 'geometry']]
-    shape_file_clear = shape_file_clear.loc[shape_file_clear['GEMEENTE'] == f'{a}']
+    shape_file_clear = shape_file_clear.loc[shape_file_clear['GEMEENTE'] == f'{ville}']
     return shape_file_clear
 
 
-def rue(y):
-    z = y
-    rue = shape_file_clear.loc[shape_file_clear['STRAATNM'] == f'{z}']
-    return rue
-
-
-def numero(n):
-    m = n
-    num = Rue.loc[Rue['HUISNR'] == f'{m}']
-    return num
-
-
-x = 'Leuven'
-y = 'Henri Regastraat'
-n = '43'
-
-
-def CRAB(x):
+def CRAB(ville, rue, numero):
     os.mkdir('CRAB_dezip')
     
     file = zipfile.ZipFile('CRAB_Adressenlijst_Shapefile.zip')
-    dezip(file)
+    file.extractall('CRAB_dezip')
     
-    a = x
-    shape_file =gpd.read_file('CRAB_dezip/Shapefile/CrabAdr.shp')
-    clear_database(shape_file)
-    
-    z = y
-    rue(y)
-    
-    m = n 
-    numero(n)
+    shape_file = gpd.read_file('CRAB_dezip/Shapefile/CrabAdr.shp')
+    shape_file_clear = clear_database(shape_file, ville)
+
+    rue = shape_file_clear.loc[shape_file_clear['STRAATNM'] == f'{rue}']
+    numero = rue.loc[rue['HUISNR'] == f'{numero}']
 
 
-CRAB(x)
+if __name__ == '__main__':
+    ville = 'Leuven'
+    rue = 'Henri Regastraat'
+    numero = '43'
+
+    CRAB(ville, rue, numero)
