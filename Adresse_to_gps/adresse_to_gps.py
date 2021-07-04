c8fd973305a8cba85152f9575a6c849fd96a4c24
AlainTiri <alaintiri@gmail.com>
2021-07-04 16:55:42 +0200

    Script de Marvin réécrit.

--- Adresse_to_gps/adresse_to_gps.py
+++ Adresse_to_gps/adresse_to_gps.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import zipfile38 as zipfile
-import os
+# import zipfile38 as zipfile
+# import os
 import geopandas as gpd
 
 
@@ -10,20 +10,23 @@ def clear_database(shape_file, ville):
 
     shape_file_clear = shape_file[['STRAATNM', 'HUISNR', 'NISCODE', 'GEMEENTE', 'geometry']]
     shape_file_clear = shape_file_clear.loc[shape_file_clear['GEMEENTE'] == f'{ville}']
+
     return shape_file_clear
 
 
 def CRAB(ville, rue, numero):
-    os.mkdir('CRAB_dezip')
+    # os.mkdir('CRAB_dezip')
     
-    file = zipfile.ZipFile('CRAB_Adressenlijst_Shapefile.zip')
-    file.extractall('CRAB_dezip')
+    # file = zipfile.ZipFile('CRAB_Adressenlijst_Shapefile.zip')
+    # file.extractall('CRAB_dezip')
     
-    shape_file = gpd.read_file('CRAB_dezip/Shapefile/CrabAdr.shp')
+    shape_file = gpd.read_file('CrabAdr.shp')
     shape_file_clear = clear_database(shape_file, ville)
 
     rue = shape_file_clear.loc[shape_file_clear['STRAATNM'] == f'{rue}']
     numero = rue.loc[rue['HUISNR'] == f'{numero}']
+
+    return numero
 
 
 if __name__ == '__main__':
@@ -31,4 +34,4 @@ if __name__ == '__main__':
     rue = 'Henri Regastraat'
     numero = '43'
 
-    CRAB(ville, rue, numero)
+    print(CRAB(ville, rue, numero))
